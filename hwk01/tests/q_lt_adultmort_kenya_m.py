a7312dc42f9604e5ae318e987e1ebeb8354de8a7
dennis feehan <feehan@berkeley.edu>
2021-02-06 09:50:50 -0800

    draft of lab 02

---
+++ hwk01/tests/q_lt_adultmort_kenya_m.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q_lt_adultmort_kenya_m',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> np.isclose(kenya_m_adultmort, .2819, atol=.01)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ lab02/tests/q_make_all_periods.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q_make_all_periods',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> len(all_periods) == 13\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> all_periods[0] == 1955\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> all_periods[12] == 2015\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
